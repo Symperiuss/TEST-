8360a321192ac35a61985de852dae6ebe6ebbbbb
Lucas JOUAN--DEPAS <lucasj35@hotmail.fr>
2024-02-13 21:44:52 +0100

    exo 09 split tout type d'entier

--- exo09_universal_int_split/exo09.ts
+++ exo09_universal_int_split/exo09.ts
@@ -1,7 +1,7 @@
 // prendre un tableau d'entier en entrée et split chaque chiffre dans un nouveau tableau
 //ex : tb 1 = [532,4 ,98, 30 , 759874], tb 2 = [5,3,2,4,9,8,3,0,7,5,9,8,7,4]
 
-function getSizeNumber(number: number){
+function getSizeNumber(number: number) {
   let h: number = 0;
   while (number > 0) {
     number = Math.floor(number / 10);
@@ -14,21 +14,18 @@ function universal_int_split(number: number[]) {
   let i: number = 0;
 
   let stock: number[] = [];
-  
- 
+
   while (i < number.length) {
     let imageArray: number = number[i];
-    
-    let countNumbers : number = getSizeNumber(imageArray)
+
+    let countNumbers: number = getSizeNumber(imageArray);
 
     let j: number = 10 ** (countNumbers - 1);
 
     while (j >= 1) {
-
       stock[stock.length] = Math.floor(imageArray / j) % 10;
-      
+
       j = Math.floor(j / 10);
-      
     }
 
     i += 1;
