cb8596d06e4b6f9ed770cf3784427344bb95a290
Lucas JOUAN--DEPAS <lucasj35@hotmail.fr>
2024-02-19 10:49:07 +0100

    update & ajout exo12

---
+++ exo12_permute_integer/exo12.ts
@@ -0,0 +1,21 @@
+// permuter deux valeurs dans un tableau
+// ex : tb1 : [0, 15 , 82 ,36 ,45] / tb2 : [0, 45, 82, 36, 15 ]
+
+function permuteInteger(array: number[], value1: number, value2: number) {
+  let stock: number = 0;
+  let stockB : number = 0;
+  let i: number = 0;
+ 
+
+  while (i < array.length) {
+    
+  }
+}
+
+
+
+let value1: number = 15;
+let value2: number = 36;
+let array: number[] = [0, 15, 82, 36, 45];
+
+console.log(permuteInteger(array, value1, value2));
